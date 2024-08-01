7b5f85515eb9cb5b96d4fa44b5aeb7a4b3f8b4b8
akash-indulkar <akashindulkar6677@gmail.com>
2024-08-01 15:23:11 +0530

    Registry for stock exchange microservices is created using eureka server

---
+++ src/test/java/com/akashxdev/StockServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashxdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
